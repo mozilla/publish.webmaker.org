a700d76d4516f09630da241d4a45460da694a8ed
Ashley Williams <ashley@bocoup.com>
2015-06-10 18:03:24 -0400

    add POST, PUT, DELETE routes and handlers for projects

--- api/modules/projects/controller.js
+++ api/modules/projects/controller.js
@@ -25,5 +25,39 @@ module.exports = {
         id: req.params.id
       }
     }).fetch());
+  },
+  createProject: function(req, reply) {
+    return reply(Project.forge({
+      title: req.payload.title,
+      user_id: req.payload.user_id,
+      tags: req.payload.tags,
+      description: req.payload.description,
+      date_created: req.payload.date_created,
+      date_updated: req.payload.date_updated
+    }).save());
+  },
+  updateProject: function(req, reply) {
+    return reply(Project.query({
+      where: {
+        id: req.payload.id
+      }
+    }).save({
+      title: req.payload.title,
+      user_id: req.payload.user_id,
+      tags: req.payload.tags,
+      description: req.payload.description,
+      date_created: req.payload.date_created,
+      date_updated: req.payload.date_updated
+    }, {
+      method: 'update',
+      patch: 'true'
+    }));
+  },
+  deleteProject: function(req, reply) {
+    return reply(Project.query({
+      where: {
+        id: req.params.id
+      }
+    }).destroy());
   }
 };
