5665bc79246bc7b9360c2769db26a48dd5ef72c7
Ashley Williams <ashley@bocoup.com>
2015-06-11 15:35:37 -0400

    add payload validation and brief docs for projects

--- api/modules/projects/routes/create.js
+++ api/modules/projects/routes/create.js
@@ -1,9 +1,14 @@
 var controller = require('../controller');
+var schema = require('../schema');
 
 module.exports = [{
   method: 'POST',
   path: '/projects',
   config: {
-    handler: controller.createProject
+    handler: controller.createProject,
+    description: 'Create a new project object.',
+    validate: {
+      payload: schema
+    }
   }
 }];
--- api/modules/projects/routes/update.js
+++ api/modules/projects/routes/update.js
@@ -1,9 +1,14 @@
 var controller = require('../controller');
+var schema = require('../schema');
 
 module.exports = [{
   method: 'PUT',
   path: '/projects/{id}',
   config: {
-    handler: controller.updateProject
+    handler: controller.updateProject,
+    description: 'Update a single project object based on `id`.',
+    validate: {
+      payload: schema
+    }
   }
 }];
