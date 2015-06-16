f95988ac8d69c43979c325767891eeb9af0325c2
ag_dubs <ashley666ashley@gmail.com>
2015-06-16 12:02:27 -0400

    add publish and unpublish routes

---
+++ api/classes/publish.js
@@ -0,0 +1,29 @@
+var AWS = require('aws-sdk'); 
+var s3 = new AWS.S3(); 
+
+var isPublished = function(project){
+  return project.get('publish_url') !== null;
+};
+
+exports.publish = function(project) {
+  var url = "FAKE://URL";
+  project.set({ publish_url:  url }).save();
+  console.log("Successfully published " + 
+          project.get('title') +
+          " to " + project.get('publish_url'));
+  return project;
+};
+
+exports.unpublish = function(project) {
+  var message = "";
+  if (isPublished(project)) {
+    project.set({ publish_url: null }).save();
+    message =  "Successfully unpublished " + project.get('title');
+  } else {
+    message = "Cannot unpublish an project that is not published.";
+  }
+  console.log(message);
+  return project;
+};
+
+module.exports = exports;
