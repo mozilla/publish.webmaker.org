ee1636ce2f009d0a0b2ba5a4a920c94b1836827a
ag_dubs <ashley666ashley@gmail.com>
2015-06-16 15:12:28 -0400

    tests for projects publish, unpublish

---
+++ test/api/handlers/projects/publish.js
@@ -0,0 +1,68 @@
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var experiment = lab.experiment;
+var test = lab.test;
+var before = lab.before;
+var after = lab.after;
+var expect = require('code').expect;
+
+var config = require("../../../lib/fixtures/projects").publish;
+var server;
+
+before(function(done) {
+  require('../../../lib/mocks/server')(function(obj) {
+    server = obj;
+
+    config(function(err, publish) {
+      if (err) throw err;
+
+      config = publish;
+      done();
+    });
+  });
+});
+
+after(function(done) {
+  server.stop(done);
+});
+
+// POST /projects/:project_id/publish
+experiment('[Publish a project]', function() {
+  test('success case', function(done) {
+    var opts = config.success.default;
+
+    server.inject(opts, function(resp) {
+      expect(resp.statusCode).to.equal(201);
+      expect(resp.result).to.exist();
+      expect(resp.result.publish_url).to.be.a.string();
+      done();
+    });
+  });
+
+  test('project must exist', function(done) {
+    var opts = config.fail.projectDoesNotExist;
+
+    server.inject(opts, function(resp) {
+      expect(resp.statusCode).to.equal(400);
+      expect(resp.result).to.exist();
+      expect(resp.result.error).to.equal('Bad Request');
+      expect(resp.result.message).to.equal("Project does not exist.");
+
+      done();
+    });
+  });
+
+  test('project_id must be a number', function(done) {
+    var opts = config.fail.projectidTypeError;
+
+    server.inject(opts, function(resp) {
+      expect(resp.statusCode).to.equal(400);
+      expect(resp.result).to.exist();
+      expect(resp.result.error).to.equal('Bad Request');
+      expect(resp.result.message).to.equal('`project_id` invalid.');
+
+      done();
+    });
+  });
+});
