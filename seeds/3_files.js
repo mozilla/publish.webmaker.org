2f94b86ed33044a12fae3cf53db654c607cf0220
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-06-17 14:32:15 -0400

    Made the test projects mirror thimble's multi-file project use case

--- seeds/3_files.js
+++ seeds/3_files.js
@@ -6,42 +6,42 @@ exports.seed = function(knex, Promise) {
   return Promise.join(
     knex('files').insert({
       project_id: 1,
-      path: 'index.js',
+      path: '/spacecats-API/index.js',
       buffer: fs.readFileSync(__dirname + '/file_data/index.js')
     }),
     knex('files').insert({
       project_id: 1,
-      path: 'package.json',
+      path: '/spacecats-API/package.json',
       buffer: fs.readFileSync(__dirname + '/file_data/package.json')
     }),
     knex('files').insert({
       project_id: 1,
-      path: 'index.html',
+      path: '/spacecats-API/index.html',
       buffer: fs.readFileSync(__dirname + '/file_data/index.html')
     }),
     knex('files').insert({
       project_id: 1,
-      path: 'public/img/sagan.jpg',
+      path: '/spacecats-API/public/img/sagan.jpg',
       buffer: fs.readFileSync(__dirname + '/file_data/public/img/sagan.jpg')
     }),
     knex('files').insert({
       project_id: 2,
-      path: 'Gemfile',
+      path: '/sinatra-contrib/Gemfile',
       buffer: fs.readFileSync(__dirname + '/file_data/Gemfile')
     }),
     knex('files').insert({
-      project_id: 1,
-      path: 'main.html',
+      project_id: 2,
+      path: '/sinatra-contrib/main.html',
       buffer: fs.readFileSync(__dirname + '/file_data/main.html')
     }),
     knex('files').insert({
       project_id: 3,
-      path: 'logo.png',
+      path: '/webmaker-android/logo.png',
       buffer: fs.readFileSync(__dirname + '/file_data/logo.png')
     }),
     knex('files').insert({
       project_id: 4,
-      path: '.gitignore',
+      path: '/makedrive/.gitignore',
       buffer: fs.readFileSync(__dirname + '/file_data/.gitignore')
     })
   );
