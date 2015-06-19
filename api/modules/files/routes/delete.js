848ada1e96c27b35eaa4f6192f9c6c6beea4341e
ag_dubs <ashley666ashley@gmail.com>
2015-06-19 13:45:57 -0400

    add route level validation for id param on DELETE /files/:id, fix test to expect correct msg

--- api/modules/files/routes/delete.js
+++ api/modules/files/routes/delete.js
@@ -1,10 +1,18 @@
 var controller = require('../controller');
+var Joi = require('joi');
+var Errors = require('../../../classes/errors');
 
 module.exports = [{
   method: 'DELETE',
   path: '/files/{id}',
   config: {
     handler: controller.delete.bind(controller),
-    description: 'Delete a single file object based on `id`.'
+    description: 'Delete a single file object based on `id`.',
+    validate: {
+      params: {
+        id: Joi.number().integer().required()
+      },
+      failAction: Errors.id
+    }
   }
 }];
