aa2192697dac27c329c4748d27c1bc226b4e82bf
ag_dubs <ashley666ashley@gmail.com>
2015-06-19 12:29:41 -0400

    add params validation to DELETE /users

--- api/modules/users/routes/delete.js
+++ api/modules/users/routes/delete.js
@@ -1,10 +1,18 @@
 var controller = require('../controller');
+var Joi = require('joi');
+var Errors = require('../../../classes/errors');
 
 module.exports = [{
   method: 'DELETE',
   path: '/users/{id}',
   config: {
     handler: controller.delete.bind(controller),
-    description: 'Delete a user object based on `id`.'
+    description: 'Delete a user object based on `id`.',
+    validate: {
+      params: {
+        id: Joi.number().integer().required()
+      },
+      failAction: Errors.id
+    }
   }
 }];
