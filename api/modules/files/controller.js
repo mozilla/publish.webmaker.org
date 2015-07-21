9c998be89b211cdf6a91316db3a56252d0c1c1ef
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-07-21 10:15:06 -0400

    Fixed #113 and #110 - Corrected file delete logic to pass tests

--- api/modules/files/controller.js
+++ api/modules/files/controller.js
@@ -1,7 +1,10 @@
 var BaseController = require('../../classes/base_controller');
+var errors = require('../../classes/errors');
 
 var Model = require('./model');
 var controller = new BaseController(Model);
+
+var PublishedFiles = require('../publishedFiles/model');
 
 controller.data = function(req) {
   var data =  {
@@ -14,5 +17,33 @@ controller.data = function(req) {
   }
   return data;
 };
+
+controller.delete = function(req, reply) {
+  var self = this;
+  var record = req.pre.records.models[0];
+
+  // If a published file exists for this file, we have
+  // to unset it's reference before deletion can occur
+  PublishedFiles.query({
+    where: {
+      file_id: record.get('id')
+    }
+  }).fetch()
+  .then(function(model) {
+    if (!model) {
+      return;
+    }
+    return model.set({
+      file_id: null
+    }).save();
+  })
+  .then(function() {
+    BaseController.prototype.delete.call(self, req, reply);
+  })
+  .catch(function(e) {
+    reply(errors.generateErrorResponse(e));
+  });
+};
+
 
 module.exports = controller;
