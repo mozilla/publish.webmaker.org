45b3fe43d5345d640326226bee0325c19cfc05c2
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-08-20 15:10:27 -0400

    Fixed mozilla/thimble.webmaker.org#893 - Ensure that publishedFiles are deleted before the project is

--- api/modules/projects/controller.js
+++ api/modules/projects/controller.js
@@ -5,6 +5,9 @@ var errors = require('../../classes/errors');
 var BaseController = require('../../classes/base_controller');
 var Publisher = require('../../classes/publisher');
 
+var Files = require('../files/model');
+var PublishedFiles = require('../publishedFiles/model');
+
 var Model = require('./model');
 var controller = new BaseController(Model);
 
@@ -57,13 +60,53 @@ controller.delete = function(req, reply) {
   var self = this;
   var project = req.pre.records.models[0];
 
+  function clearAllPublishedFiles() {
+    function fetchFiles() {
+      return Files.query({
+        where: {
+          project_id: project.get('id')
+        }
+      }).fetchAll();
+    }
+
+    function fetchPublishedFiles(file) {
+      return PublishedFiles.query({
+        where: {
+          file_id: file.get('id')
+        }
+      }).fetchAll();
+    }
+
+    function clearPublishedFiles(publishedFiles) {
+      if (publishedFiles.length === 0) {
+        return;
+      }
+      return publishedFiles.mapThen(function(publishedFile) {
+        return publishedFile.destroy();
+      });
+    }
+
+    return Promise.resolve()
+      .then(fetchFiles)
+      .then(function(files) {
+        return files.mapThen(function(file) {
+          return fetchPublishedFiles(file)
+            .then(clearPublishedFiles);
+        });
+      });
+  }
+
   // If this project is published, we have to
   // unpublish it before it can be safely deleted.
+  // We then do a dummy check to make sure no old publishedFiles
+  // exist for this. Horribly inefficent!
+  // TODO: https://github.com/mozilla/publish.webmaker.org/issues/140
   Promise.resolve().then(function() {
     if (project.get('published_id')) {
       return Publisher.unpublish(project);
     }
   })
+  .then(clearAllPublishedFiles)
   .then(function() {
     BaseController.prototype.delete.call(self, req, reply);
   })
