63e2894e93d0b26e583ad72100dcd633314d3041
Jon Buckley <jon@jbuckley.ca>
2015-09-11 14:40:11 -0400

    Newrelic needs to be the first module loaded

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require('newrelic');
+
 var Hapi = require('hapi');
 var Hoek = require('hoek');
 
 // Initialize the environment before anything else happens
 require('./lib/environment');
-
-require('newrelic');
 
 Hoek.assert(process.env.API_HOST, 'Must define API_HOST');
 Hoek.assert(process.env.PORT, 'Must define PORT');
