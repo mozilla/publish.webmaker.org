37b7144f88d33a0726a84ec95ea034bcf621706e
Gideon Thomas <r.gideonthomas@gmail.com>
2015-11-19 16:08:43 -0500

    Also add entries into the _date column when added to the date column for projects and publishedProjects

--- api/modules/publishedProjects/model.js
+++ api/modules/publishedProjects/model.js
@@ -13,6 +13,18 @@ var instanceProps = {
   publishedFiles: function() {
     return this.hasMany(require('../publishedFiles/model'));
   },
+  format: function(model) {
+    if (typeof model === 'object') {
+      if (model.date_created) {
+        model._date_created = new Date(model.date_created);
+      }
+      if (model.date_updated) {
+        model._date_updated = new Date(model.date_updated);
+      }
+    }
+
+    return model;
+  },
   parse: function(model) {
     if (typeof model === 'object') {
       delete model._date_created;
