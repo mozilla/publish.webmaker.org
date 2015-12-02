320377732da3092b5eac4233ce34cc9fe34a4c2d
Gideon Thomas <r.gideonthomas@gmail.com>
2015-12-02 12:04:58 -0500

    Drop text date columns

--- test/lib/fixtures/publishedProjects/test-publishedProjects.js
+++ test/lib/fixtures/publishedProjects/test-publishedProjects.js
@@ -15,6 +15,16 @@ module.exports = function(callback) {
 
   db.select().table('publishedProjects').orderBy('id')
   .then(function(rows) {
+    rows.forEach(function(row) {
+      if (row._date_created) {
+        row.date_created = row._date_created.toISOString();
+        delete row._date_created;
+      }
+      if (row._date_updated) {
+        row.date_updated = row._date_updated.toISOString();
+        delete row._date_updated;
+      }
+    });
     publishedProjects.valid = rows;
     callback(null, publishedProjects);
   })
