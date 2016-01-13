c7f07ef1ea1aec8f9e93eeba62f6f077bf07c2b7
Christopher De Cairos <cade@mozillafoundation.org>
2016-01-13 13:18:02 -0500

    transacting!

--- scripts/import-thimble-projects.js
+++ scripts/import-thimble-projects.js
@@ -1,6 +1,8 @@
 // import-thimble-projects.js
 // Imports old thimble projects into a publish database
 //
+// DETAILED MIGRATION INSTRUCTIONS: https://gist.github.com/cadecairos/44e3d34845d0f438779a#file-thimblemigration-md
+//
 // execute as:
 // node scripts/import-thimble-projects.js <legacyProjectConnString> <publishConnString>
 
@@ -48,31 +50,34 @@ function checkAndAddUsers() {
 function mapUsers(users) {
   return publishClient.transaction((txn) => {
     return Promise.all(
-      users.map(checkExistence)
+      users.map(checkExistence, txn)
     )
     .then(txn.commit)
-    .catch(txn.rollback)
+    .catch(txn.rollback);
   });
 }
 
 function checkExistence(user) {
   return publishClient('users')
+  var txn = this;
   .select()
   .where('name', '=', user.username)
+  .transacting(txn)
   .then(function(result) {
     if (result.length === 0) {
-      return insertPublishUser(user);
+      return insertPublishUser(user, txn);
     }
     publishUserIdsByEmail[user.email] = result[0].id;
   });
 }
 
-function insertPublishUser(user) {
+function insertPublishUser(user, txn) {
   return publishClient('users')
   .insert({
     name: user.username
   })
   .returning('id')
+  .transacting(txn)
   .then(function(result) {
     publishUserIdsByEmail[user.email] = result[0];
   });
@@ -96,7 +101,7 @@ function migrateOldProjects(oldProjects) {
 function handleOldProjects(oldProjects) {
   return publishClient.transaction((txn) => {
     Promise.all(
-      oldProjects.map(migrateProject)
+      oldProjects.map(migrateProject, txn)
     )
     .then(txn.commit)
     .catch(txn.rollback);
@@ -104,11 +109,14 @@ function handleOldProjects(oldProjects) {
 }
 
 function migrateProject(oldProject) {
-  return createPublishProject(oldProject)
-  .then(createPublishFile);
+  // this === txn
+  return createPublishProject(oldProject, this)
+  .then((projectData) => {
+    createPublishFile(projectData, this);
+  });
 }
 
-function createPublishProject(oldProject) {
+function createPublishProject(oldProject, txn) {
   return publishClient('projects')
   .insert({
     title: oldProject.title,
@@ -117,6 +125,7 @@ function createPublishProject(oldProject) {
     user_id: publishUserIdsByEmail[oldProject.email]
   })
   .returning('id')
+  .transacting(txn)
   .then((result) => {
     return {
       publishId: result[0],
@@ -125,11 +134,12 @@ function createPublishProject(oldProject) {
   });
 }
 
-function createPublishFile(projectData) {
+function createPublishFile(projectData, txn) {
   return publishClient('files')
   .insert({
     path: '/index.html',
     project_id: projectData.publishId,
     buffer: new Buffer(projectData.oldProject.html)
-  });
+  })
+  .transacting(txn);
 }
