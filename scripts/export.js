9c7a72daf2ea94ef0d21e1e429aa6c996927abd7
Gideon Thomas <gideon@mozillafoundation.org>
2016-06-24 11:29:25 -0400

    [ES6] Scripts

--- scripts/export.js
+++ scripts/export.js
@@ -1,92 +1,92 @@
-var Promise = require('bluebird');
-var program = require('commander');
-var pgClient = require('pg').Client;
-var Tar = require('tar-stream');
-var colors = require('colors');
-var fs = Promise.promisifyAll(require('fs'));
-var path = require('path');
+"use strict";
 
-var client;
-var projectID;
-var project;
-var destination;
+const Promise = require(`bluebird`);
+const program = require(`commander`);
+const PgClient = require(`pg`).Client;
+const Tar = require(`tar-stream`);
+const colors = require(`colors`);
+const fs = Promise.promisifyAll(require(`fs`));
+const path = require(`path`);
+
+let client;
+let projectID;
+let project;
+let destination;
 
 colors.setTheme({
-  silly: 'rainbow',
-  input: 'grey',
-  verbose: 'cyan',
-  prompt: 'grey',
-  info: 'green',
-  data: 'grey',
-  help: 'cyan',
-  warn: 'yellow',
-  debug: 'blue',
-  error: 'red'
+  silly: `rainbow`,
+  input: `grey`,
+  verbose: `cyan`,
+  prompt: `grey`,
+  info: `green`,
+  data: `grey`,
+  help: `cyan`,
+  warn: `yellow`,
+  debug: `blue`,
+  error: `red`
 });
 
 function getMessage() {
-  var message = '';
-  Array.prototype.forEach.call(arguments, function(arg) {
-    if(arg) {
-      message += arg + ' ';
-    }
-  });
-  message = message.trim();
-  return message;
+  return Array.from(arguments).filter(arg => !!arg).join(' ').trim();
 }
 
-var log = {
-  error: function() {
-    var message = getMessage.apply(null, arguments);
-    console.error(('ERROR: ' + message).error);
-    console.error('\n\n');
+class Log {
+  static error() {
+    const message = getMessage.apply(null, arguments);
+    console.error((`ERROR: ${message}`).error);
+    console.error(`\n\n`);
     process.exit(1);
-  },
-  info: function() {
-    var message = getMessage.apply(null, arguments);
+  }
+
+  static info() {
+    const message = getMessage.apply(null, arguments);
     console.log(message.verbose);
-  },
-  success: function() {
-    var message = getMessage.apply(null, arguments);
+  }
+
+  static success() {
+    const message = getMessage.apply(null, arguments);
     console.log(message.info);
   }
-};
+}
 
 function validate() {
-  var errorMsg = false;
-  var missingError = ' must be provided!\n';
+  let errorMsg = false;
+  const missingError = `must be provided!\n`;
 
-  if (typeof program.args[0] === 'undefined') {
-    errorMsg = 'Project ID' + missingError;
-  } else if (typeof program.username === 'undefined') {
-    errorMsg = 'Username' + missingError;
-  } else if (typeof program.database === 'undefined') {
-    errorMsg = 'Database name' + missingError;
+  if (typeof program.args[0] === `undefined`) {
+    errorMsg = `Project ID ${missingError}`;
+  } else if (typeof program.username === `undefined`) {
+    errorMsg = `Username ${missingError}`;
+  } else if (typeof program.database === `undefined`) {
+    errorMsg = `Database name ${missingError}`;
   }
 
   projectID = Number(program.args[0]);
   if (isNaN(projectID)) {
-    errorMsg = 'Project ID must be a number!\n';
+    errorMsg = `Project ID must be a number!\n`;
   }
 
   if (errorMsg) {
-    log.error(errorMsg);
+    Log.error(errorMsg);
   }
 }
 
 function writeProjectDetailsFile(project) {
-  return fs.writeFileAsync(path.join(destination, 'project.txt'), JSON.stringify(project, null, 4))
-  .then(log.info.bind(log, 'Created project details file...'))
+  return fs.writeFileAsync(
+    path.join(destination, `project.txt`),
+    JSON.stringify(project, null, 4)
+  )
+  .then(() => Log.info(`Created project details file...`))
   .catch(function(err) {
-    log.error(
-      'Failed to write file with details about project with:',
+    Log.error(
+      `Failed to write file with details about project with:`,
       JSON.stringify(err, null, 2)
     );
   });
 }
 
-process.on('exit', function(code) {
-  if(client) {
+process.on(`exit`, function(code) {
+  if (client) {
     client.end();
   }
 
@@ -94,22 +94,22 @@ process.on('exit', function(code) {
 });
 
 program
-  .usage('<project_id> [options]')
-  .description('A utility to export a project or it\'s published version from the publish database as a tarball')
-  .option('-u, --username <value>', 'DB username')
-  .option('-p, --password [value]', 'DB password')
-  .option('-d, --database <value>', 'DB name')
-  .option('-h, --host [value]', 'DB host server (without the protocol), defaults to localhost', 'localhost')
-  .option('-D, --destination [path]', 'Directory to save the project tarball in, defaults to the current directory', process.cwd())
-  .option('-P, --published', 'Get the published version instead of the WIP project')
-  .option('-i, --include-project', 'Provide a file with details about the project itself such as the name, description, etc. along with the tarball')
-  .parse(process.argv);
+.usage(`<project_id> [options]`)
+.description(`A utility to export a project or it's published version from the publish database as a tarball`)
+.option(`-u, --username <value>`, `DB username`)
+.option(`-p, --password [value]`, `DB password`)
+.option(`-d, --database <value>`, `DB name`)
+.option(`-h, --host [value]`, `DB host server (without the protocol), defaults to localhost`, `localhost`)
+.option(`-D, --destination [path]`, `Directory to save the project tarball in, defaults to the current directory`, process.cwd())
+.option(`-P, --published`, `Get the published version instead of the WIP project`)
+.option(`-i, --include-project`, `Provide a file with details about the project itself such as the name, description, etc. along with the tarball`)
+.parse(process.argv);
 
 validate();
 
 destination = path.resolve(process.cwd(), program.destination);
 
-client = Promise.promisifyAll(new pgClient({
+client = Promise.promisifyAll(new PgClient({
   user: program.username,
   database: program.database,
   password: program.password,
@@ -119,15 +119,15 @@ client = Promise.promisifyAll(new pgClient({
 
 client.connectAsync()
 .then(function() {
-  var cols = [
-    'title', 'description', 'publish_url', 'published_id',
-    'client', '_date_created', '_date_updated'
+  const cols = [
+    `title`, `description`, `publish_url`, `published_id`,
+    `client`, `_date_created`, `_date_updated`
   ];
 
   return client.queryAsync(
-    'SELECT ' + '"' + cols.join('", "') + '" ' +
-    'FROM "projects" ' +
-    'WHERE "id" = $1',
+    `SELECT "` + cols.join('", "') + `" ` +
+    `FROM "projects" ` +
+    `WHERE "id" = $1`,
     [ projectID ]
   )
   .then(function(result) {
@@ -143,26 +143,26 @@ client.connectAsync()
     return Promise.resolve();
   })
   .catch(function(err) {
-    log.error(
-      'Failed to acquire project from the database with:',
+    Log.error(
+      `Failed to acquire project from the database with:`,
       JSON.stringify(err, null, 2)
     );
   });
 })
 .then(function() {
-  var table = program.published ? '"publishedFiles"' : '"files"';
-  var fk = program.published ? '"published_id"' : '"project_id"';
-  var id = program.published ? project.published_id : projectID;
-  var cols = [ 'path', 'buffer' ];
+  const table = program.published ? `"publishedFiles"` : `"files"`;
+  const fk = program.published ? `"published_id"` : `"project_id"`;
+  const id = program.published ? project.published_id : projectID;
+  const cols = [ `path`, `buffer` ];
 
   return client.queryAsync(
-    'SELECT ' + '"' + cols.join('", "') + '" ' +
-    'FROM ' + table + ' ' +
-    'WHERE ' + fk + ' = $1',
+    `SELECT "${cols.join('", "')}"` +
+    `FROM ${table} ` +
+    `WHERE ${fk} = $1`,
     [ id ]
   )
   .then(function(result) {
-    var tarStream = Tar.pack();
+    const tarStream = Tar.pack();
 
     function processFile(fileRow) {
       return new Promise(function(resolve) {
@@ -176,26 +176,28 @@ client.connectAsync()
       tarStream.finalize();
 
       return new Promise(function(resolve, reject) {
-        var writeStream = fs.createWriteStream(path.join(destination, project.title + '.tar'));
+        const writeStream = fs.createWriteStream(
+          path.join(destination, `${project.title}.tar`)
+        );
 
-        writeStream.on('finish', resolve);
+        writeStream.on(`finish`, resolve);
         tarStream.pipe(writeStream);
       });
     });
   })
-  .then(log.info.bind(log, 'Finished tarring the project...'))
+  .then(Log.info.bind(Log, `Finished tarring the project...`))
   .catch(function(err) {
-    log.error(
-      'Failed to tar the project with:',
+    Log.error(
+      `Failed to tar the project with:`,
       JSON.stringify(err, null, 2)
     );
   });
 })
-.then(log.success.bind(log, 'Successfully exported project to ' + destination + '\n\n'))
+.then(Log.success.bind(Log, `Successfully exported project to ${destination}\n\n`))
 .then(process.exit.bind(process, 0))
 .catch(function(err) {
-  log.error(
-    'FATAL - ',
+  Log.error(
+    `FATAL - `,
     JSON.stringify(err, null, 2)
   );
 });
