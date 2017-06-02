a2e93ecbf35d418205e0361499df541533e31667
Gideon Thomas <gideon@mozillafoundation.org>
2017-06-02 17:24:24 -0400

    Cache file buffers manually on remix

--- api/modules/files/cache.js
+++ api/modules/files/cache.js
@@ -4,7 +4,25 @@ const Files = require(`./model`);
 
 const BaseCache = require(`../../classes/base_cache`);
 
+const DEFAULT_BUFFER_CACHE_EXPIRY_SEC = 10 * 60;
+
+function getBuffer(fileId, next) {
+  return Files.query({
+    where: {
+      id: fileId
+    },
+    columns: [`buffer`]
+  })
+  .fetch()
+  .then(file => next(null, file && file.get(`buffer`)))
+  .catch(next);
+}
+
 class RemixedFileCreationCache extends BaseCache {
+  constructor(cache) {
+    super(cache);
+  }
+
   get name() {
     return `createdRemixFile`;
   }
@@ -15,25 +33,44 @@ class RemixedFileCreationCache extends BaseCache {
     return null;
   }
 
+  /*
+   * This method takes an optional `fileBuffer` argument
+   * If the `fileBuffer` was passed in, it writes it to cache
+   * If not, the callback is called with a `fileBuffer` retrieved from cache
+   */
   run(fileId, fileBuffer, next) {
+    /*
+     * Putting the file buffer into cache
+     */
 
     if(typeof fileBuffer !== `function`) {
-      return next(null, fileBuffer);
+      if(!this.cache) {
+        return next(null, fileBuffer);
+      }
+
+      return this.cache.setex(`file:${fileId}`, DEFAULT_BUFFER_CACHE_EXPIRY_SEC, fileBuffer)
+      .then(() => next(null, fileBuffer))
+      .catch(next);
     }
 
     next = fileBuffer;
     fileBuffer = null;
 
-    return Files.query({
-      where: {
-        id: fileId
-      },
-      columns: [`buffer`]
-    })
-    .fetch()
-    .then(file => {
-      console.log(`Typeof buffer: `, typeof file.get(`buffer`));
-      next(null, file && file.get(`buffer`));
+    /*
+     * Getting the file buffer from cache
+     */
+
+    if(!this.cache) {
+      return getBuffer(fileId, next);
+    }
+
+    this.cache.getBuffer(`file:${fileId}`)
+    .then(cachedFileBuffer => {
+      if (!cachedFileBuffer) {
+        return getBuffer(fileId, next);
+      }
+
+      next(null, cachedFileBuffer);
     })
     .catch(next);
   }
